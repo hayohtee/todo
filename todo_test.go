d0ff88c076ede461a832b933c444b198bb4da9b5
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-17 22:31:46 +0100

    Add TestComplete

--- todo_test.go
+++ todo_test.go
@@ -16,3 +16,20 @@ func TestAdd(t *testing.T) {
 		t.Errorf("expected %q, got %q instead", taskName, todoList[0].Task)
 	}
 }
+
+func TestComplete(t *testing.T) {
+	todoList := todo.TodoList{}
+
+	taskName := "New Task"
+	todoList.Add(taskName)
+
+	if todoList[0].Task != taskName {
+		t.Errorf("expected %q, got %q instead", taskName, todoList[0].Task)
+	}
+
+	todoList.Complete(1)
+
+	if !todoList[0].Done {
+		t.Errorf("new task should be completed")
+	}
+}
