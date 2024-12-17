e5923a412e2fa11f448f14baa79cb48885b375fe
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-17 21:58:16 +0100

    Add Complete method

--- todo.go
+++ todo.go
@@ -1,6 +1,9 @@
 package todo
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // item is a struct that contains the fields for a todo item.
 type item struct {
@@ -23,4 +26,19 @@ func (t *TodoList) Add(task string) {
 	}
 
 	*t = append(*t, todoItem)
+}
+
+// Complete is a method that marks a todo item as completed
+// by setting Done = true and CompletedTime to current time.
+func (t *TodoList) Complete(i int) error {
+	todoSlice := *t
+	if i <= 0 || i > len(todoSlice) {
+		return fmt.Errorf("item %d does not exist", i)
+	}
+
+	// Adjust the index for 0 based index
+	todoSlice[i-1].Done = true
+	todoSlice[i-1].CompletedAt = time.Now()
+
+	return nil
 }
