5fd57de6db8c7bc2ac2141c8e123f14ad574a8d6
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-17 22:27:47 +0100

    Add TestAdd function

---
+++ todo_test.go
@@ -0,0 +1,18 @@
+package todo_test
+
+import (
+	"testing"
+
+	"github.com/hayohtee/todo"
+)
+
+func TestAdd(t *testing.T) {
+	todoList := todo.TodoList{}
+
+	taskName := "New Task"
+	todoList.Add(taskName)
+
+	if todoList[0].Task != taskName {
+		t.Errorf("expected %q, got %q instead", taskName, todoList[0].Task)
+	}
+}
