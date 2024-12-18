46c91d7bdc71cccd646278afdaa6a52c2b0d180c
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-12-18 18:30:23 +0100

    Update TestTodoCLI

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -63,8 +63,8 @@ func TestTodoCLI(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		expected := task + "\n"
-
+		expected := fmt.Sprintf(" 1: %s\n", task)
+		
 		if expected != string(out) {
 			t.Errorf("expected %q, got %q instead\n", expected, string(out))
 		}
