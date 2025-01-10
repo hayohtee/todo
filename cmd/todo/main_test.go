effe0924dae15e934fd3ee89ddee10b0d07391bf
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-10 13:52:44 +0100

    Move the code from main_test.go to main.go

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -1,12 +1,1 @@
 package main
-
-import "time"
-
-// todo is a struct containing information about 
-// a particular todo item.
-type todo struct {
-	Task      string
-	Done      bool
-	CreatedAt time.Time
-	CompletedAt time.Time
-}
