f44b888b3255f7677ea8de05dceb455a90349c66
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-10 13:59:10 +0100

    Move the file from main.go to the appropriate file

--- todo.go
+++ todo.go
@@ -1,1 +1,27 @@
 package todo
+
+import "time"
+
+// todo is a struct containing information about
+// a particular todo item.
+type todo struct {
+	Task        string
+	Done        bool
+	CreatedAt   time.Time
+	CompletedAt time.Time
+}
+
+// TodoList is a custom type with the underlying []todo data type.
+// It contains various methods for creating and managing todos.
+type TodoList []todo
+
+// Add creates a new todo item and appends it to the list.
+func (l *TodoList) Add(task string) {
+	t := todo{
+		Task:      task,
+		CreatedAt: time.Now(),
+		Done:      false,
+	}
+
+	*l = append(*l, t)
+}
