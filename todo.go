0c115c5f4e7dc7b9323ed8d769a13d3746288f1d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-10 14:13:51 +0100

    Add Delete method

--- todo.go
+++ todo.go
@@ -34,15 +34,30 @@ func (t *TodoList) Add(task string) {
 // It sets Done field to true and CompletedAt field to the current time
 func (t *TodoList) Complete(pos int) error {
 	todoList := *t
-	
+
 	// Check if pos is within the range of todo list.
-	if pos <= 0 || pos >= len(todoList) {
+	if pos <= 0 || pos > len(todoList) {
 		return fmt.Errorf("item %d does not exist", pos)
 	}
 
 	// Adjusting for 0-based index.
-	todoList[pos - 1].Done = true
-	todoList[pos - 1].CompletedAt = time.Now()
+	todoList[pos-1].Done = true
+	todoList[pos-1].CompletedAt = time.Now()
 
+	return nil
+}
+
+// Delete a todo item from the todo list based on the
+// provided position in the list.
+func (t *TodoList) Delete(pos int) error {
+	todoList := *t
+
+	// Check if pos is within the range of todo list.
+	if pos <= 0 || pos > len(todoList) {
+		return fmt.Errorf("item %d does not exist", pos)
+	}
+
+	// Adjusting for 0-based index.
+	*t = append(todoList[:pos-1], todoList[:pos]...)
 	return nil
 }
