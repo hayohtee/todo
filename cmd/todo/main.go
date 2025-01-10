ca844c17cfe775ead43c1f29874418478c50cb8d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-10 13:58:32 +0100

    Add a Add method on the TodoList

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -14,3 +14,15 @@ type todo struct {
 // TodoList is a custom type with the underlying []todo data type.
 // It contains various methods for creating and managing todos.
 type TodoList []todo
+
+
+// Add creates a new todo item and appends it to the list.
+func (l *TodoList) Add(task string) {
+	t := todo{
+		Task: task,
+		CreatedAt: time.Now(),
+		Done: false,
+	}
+
+	*l = append(*l, t)
+}
