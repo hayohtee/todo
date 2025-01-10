a5bc71b96936746ecb662d4a826322b6fd8643bd
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-10 14:50:51 +0100

    Add TestComplete method for testing TodoList Complete method

--- todo_test.go
+++ todo_test.go
@@ -16,3 +16,24 @@ func TestAdd(t *testing.T) {
 		t.Errorf("expected: %q, got %q instead", taskName, todoList[0].Task)
 	}
 }
+
+func TestComplete(t *testing.T) {
+	var todoList todo.TodoList
+
+	taskName := "New Task"
+	todoList.Add(taskName)
+
+	if todoList[0].Task != taskName {
+		t.Errorf("expected: %q, got %q instead", taskName, todoList[0].Task)
+	}
+
+	if todoList[0].Done {
+		t.Error("new task should not be completed")
+	}
+
+	todoList.Complete(1)
+
+	if !todoList[0].Done {
+		t.Error("new task should be completed")
+	}
+}
