23cf0ea45ff06c0ee45a336637eacc6b723d9ac2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-10 15:05:47 +0100

    Add TestDelete function for testing TodoList Delete method

--- todo_test.go
+++ todo_test.go
@@ -37,3 +37,31 @@ func TestComplete(t *testing.T) {
 		t.Error("new task should be completed")
 	}
 }
+
+func TestDelete(t *testing.T) {
+	var todoList todo.TodoList
+
+	tasks := []string{
+		"New Task 1",
+		"New Task 2",
+		"New Task 3",
+	}
+
+	for _, v := range tasks {
+		todoList.Add(v)
+	}
+
+	if todoList[0].Task != tasks[0] {
+		t.Errorf("expected: %q, got %q instead", tasks[0], todoList[0].Task)
+	}
+
+	todoList.Delete(2)
+
+	if len(todoList) != 2 {
+		t.Errorf("expected list length: %d, got %d instead", 2, len(todoList))
+	}
+
+	if todoList[1].Task != tasks[2] {
+		t.Errorf("expected: %q, got %q instead", tasks[2], todoList[1].Task)
+	}
+}
