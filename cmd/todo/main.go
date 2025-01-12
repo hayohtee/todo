6ae2a047562f55d9e19f2b7a2e693fe399869ed8
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-12 18:07:13 +0100

    Add switch statement to decide what operation to perform based on the length of arguments provided

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/hayohtee/todo"
 )
@@ -15,5 +16,19 @@ func main() {
 	if err := todoList.Get(todoFileName); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
+	}
+
+	switch {
+	case len(os.Args) == 1:
+		for _, item := range todoList {
+			fmt.Println(item.Task)
+		}
+	default:
+		item := strings.Join(os.Args[1:], " ")
+		todoList.Add(item)
+		if err := todoList.Save(item); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 	}
 }
