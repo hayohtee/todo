72f38545c37bc65f3ecc640a66e29b0a505655b9
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2025-01-12 18:03:00 +0100

    Declare todoList variable and attempt to read from file into the TodoList

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/hayohtee/todo"
+)
+
+const todoFileName = ".todo.json"
+
+func main() {
+	var todoList todo.TodoList
+
+	if err := todoList.Get(todoFileName); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
